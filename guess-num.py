8b749f12b9fef434d81bb8ec817d4a475e92f52b
kennychang68 <a0987939193@gmail.com>
2022-10-04 16:30:29 +0800

    add count

--- guess-num.py
+++ guess-num.py
@@ -1,21 +1,20 @@
 import random
 r = random.randint (1, 10)
 print(r)
-#value = input('please type your number :')
-#value = int(value)
+count = 0
 while True:
+	count = count + 1
 	value = input('please type your number :')
 	value = int(value)
 	if value == r:
 		print('matched')
+		print('you have tried', count, 'time')
 		break
 	else:
 		if value > r:
 			print('r is smaller')
 		else:
 			print('r is larger')
-		print('please try again : ')
-
-
+	print('you have tried', count, 'time')
 
 
